8112bf95e36b9e2b226a85930b04efe84f923bd6
Tharmi1305 <139675003+Tharmi1305@users.noreply.github.com>
2025-03-15 05:29:25 +0000

    print the numbers 1 to 10 in the reverse order

---
+++ Three.java
@@ -0,0 +1,12 @@
+//print the numbers 1 to 10 in reverse
+
+public class Three 
+{
+    public static void main(String args[])
+    {
+        for(int i=10;i>0;i--)
+        {
+            System.out.println(i);
+        }
+    }   
+}
