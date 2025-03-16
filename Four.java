6adc5f1d8ef840e33cf377f143fa59b196fd1964
Tharmi1305 <139675003+Tharmi1305@users.noreply.github.com>
2025-03-16 14:22:31 +0000

    find the odd numbers between 1 to 20

---
+++ Four.java
@@ -0,0 +1,15 @@
+//print the odd numbers between 1 to 20
+
+public class Four
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=20;i++)
+        {
+            if (i % 2==1)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
